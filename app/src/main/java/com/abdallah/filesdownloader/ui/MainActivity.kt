44f19a0e64ba32dc45a2ec287942ce076d9bbbb7
Abdallah <abdallah.elsyd.sh@gmail.com>
2022-04-25 05:05:34 +0200

    setup structure add required dependencies

--- app/src/main/java/com/abdallah/filesdownloader/ui/MainActivity.kt
+++ app/src/main/java/com/abdallah/filesdownloader/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.abdallah.filesdownloader
+package com.abdallah.filesdownloader.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.abdallah.filesdownloader.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
